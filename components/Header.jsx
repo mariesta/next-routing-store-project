2681c13e50e0649c4e899c8ebae80d5977464e4a
Marie Starck <marie.compsci14@gmail.com>
2020-11-27 13:24:53 +0100

    Routing implemented

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,17 @@
-export default function Header() {
+import { withRouter } from 'next/router'
+
+function Header({router}) {
+  const navigateHome = () => router.push("/")
+
   return(
     <header className="store-header py-3">
       <div className="row flex-nowrap justify-content-between align-items-center">
         <div className="col-12 text-center">
-          <a className="store-header-logo text-dark" href="#">My Store</a>
+          <a className="store-header-logo text-dark" onClick={navigateHome}>My Store</a>
         </div>
       </div>
     </header>
   )
 }
+
+export default withRouter(Header)
